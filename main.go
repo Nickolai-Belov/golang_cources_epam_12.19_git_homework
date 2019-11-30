34811b71e1e241063216b2e502cbbf3fd58d104b
VSKut <me@vskut.ru>
2019-11-30 21:05:40 +0300

    Revert "​owner_name, adding weather question"
    This reverts commit a592d6ce

--- main.go
+++ main.go
@@ -1,22 +1,8 @@
-package golanghome
+package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("please enter your name:")
-	inputScanner := bufio.NewScanner(os.Stdin)
-	inputScanner.Scan()
-	name := inputScanner.Text()
-
-	fmt.Println("Hello", name)
-	fmt.Println("How is the weather today?")
-	inputScanner.Scan()
-	weather := inputScanner.Text()
-	fmt.Println("Weather today:", weather)
 	const Brave = "brave"
 	const New = "new"
 
