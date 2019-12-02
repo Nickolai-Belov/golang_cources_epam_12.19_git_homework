723153796654abac7f1254b31727b090f549bd30
John P <johnp334334334@gmail.com>
2019-12-03 00:23:45 +0300

    add_adjective
    add some const to console output

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"os"
 )
 
+const (
+	BraveAdj = "brave"
+	NewAdj = "new"
+)
+
 func main() {
 	var input string
 	fmt.Print("Please enter your name: ")
@@ -12,6 +17,6 @@ func main() {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	fmt.Println("Hello", input)
+	fmt.Println("Hello", BraveAdj, NewAdj, input)
 	fmt.Println("How is the weather today?")
 }
