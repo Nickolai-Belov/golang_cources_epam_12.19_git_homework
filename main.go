e87ef940a4b14a2a21d1be4a55138a3aa75d2ef1
John P <johnp334334334@gmail.com>
2019-12-03 00:02:49 +0300

    owner_name
    add name input from console and question about the weather

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var input string
+	fmt.Print("Please enter your name: ")
+	_, err := fmt.Scan(&input)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	fmt.Println("Hello", input)
+	fmt.Println("How is the weather today?")
 }
