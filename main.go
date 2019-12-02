8ff20954042083d6593ee30a906dd1758bcd205f
Egor Geller <Egkingers11@gmail.com>
2019-12-02 18:03:20 +0300

    owner_name added user input string

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	str,_ := reader.ReadString('\n')
+	fmt.Printf("Hello %s", str)
 }
