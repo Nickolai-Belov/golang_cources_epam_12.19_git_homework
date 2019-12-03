85aa0e383bf542ae88da945ae64a3a5eef7c6b9f
Vladislav Kulizhenko <alienv661@gmail.com>
2019-12-04 00:45:42 +0300

    Reset master

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	const Brave, New string = " brave ", "new "
 	var name string
 	fmt.Print("please enter your name: ")
 	fmt.Scan(&name)
-	fmt.Println("Hello ", name)
+	fmt.Println(name)
+	fmt.Println("Hello", Brave, New, name)
 	fmt.Println("How is the weather today?")
 }
