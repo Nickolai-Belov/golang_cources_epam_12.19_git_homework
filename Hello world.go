7456c80c925be44c7647cbcbbaf7608a56a5403b
Yuriy Lukashewich <yura2000uraura@gmail.com>
2019-12-03 14:54:12 +0300

    and question about the weather

--- Hello world.go
+++ Hello world.go
@@ -9,5 +9,6 @@ func main()  {
 	var name string
 	fmt.Printf("Please enter your name: ")
 	_, _ = fmt.Fscan(os.Stdin, &name)
-	fmt.Println("Hello brave new ", name)
+	fmt.Println("Hello, ", name)
+	fmt.Printf("How is the weather today?")
 }
