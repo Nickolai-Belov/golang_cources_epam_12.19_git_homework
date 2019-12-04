ea51db9559763cf596c5fd7de65c8cd0523e0c14
cryptokitty <19489650+injectz@users.noreply.github.com>
2019-12-04 20:54:20 +0300

    Revert "owner_name"
    This reverts commit 7ed48fb7c2a0202ccbce240ab9a7d5fa872f391b.

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("please enter your name: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Printf("%s", text)
-	fmt.Printf("Hello brave new%s", text)
-	fmt.Println("How is the weather today?")
+  fmt.Println("Hello brave new world")
 }
