76bff1e5bedce1a1d38b6f636fdca2dca65c1821
John P <johnp334334334@gmail.com>
2019-12-04 21:20:08 +0300

    Revert "owner_name"
    This reverts commit e87ef940

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-)
-
-const (
-	BraveAdj = "brave"
-	NewAdj = "new"
 )
 
 func main() {
-	var input string
-	fmt.Print("Please enter your name: ")
-	_, err := fmt.Scan(&input)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	fmt.Println("Hello", BraveAdj, NewAdj, input)
-	fmt.Println("How is the weather today?")
-
+	fmt.Println("Hello World")
 }
