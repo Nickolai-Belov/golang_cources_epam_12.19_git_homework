33ba87803f29e4945dd6471ce19dd8e7b53de304
Yuriy Lukashewich <yura2000uraura@gmail.com>
2019-12-05 17:08:57 +0300

    Mistake correction
    it was Hello, name instead of Hello brave new name
    a little mistake in process of rebasing

--- Hello world.go
+++ Hello world.go
@@ -9,7 +9,7 @@ func main()  {
 	var name string
 	fmt.Printf("Please enter your name: ")
 	_, _ = fmt.Fscan(os.Stdin, &name)
-	fmt.Println("Hello, ", name)
+	fmt.Println("Hello brave new ", name)
 	fmt.Printf("How is the weather today?")
 	//weather asking
 }
