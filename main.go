0553261f86b5d45f0567ab20b4d8a7cc5f15e288
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2019-12-05 22:38:10 +0300

    owner_name Added console input for "name" variable and weather question

--- main.go
+++ main.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func main() {
 	const b, n = "brave", "new"
-	fmt.Printf("Hello %s %s world", b, n)
+	name := ""
+	fmt.Println("Enter your name:")
+	fmt.Scan(&name)
+	fmt.Printf("Hello %s %s %s\n", b, n, name)
+	fmt.Println("How is the weather today?")
 }
