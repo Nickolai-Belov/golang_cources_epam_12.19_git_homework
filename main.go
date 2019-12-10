3bd65951a269f36a5f96888d3584d4c1c27be57c
Ste Zha <stephenzhavrid@gmail.com>
2019-12-10 11:45:44 +0300

    owner_name amend 8, now is 9

--- main.go
+++ main.go
@@ -6,4 +6,9 @@ func main() {
 	const br = "brave"
 	const ne = "new"
 	fmt.Println("Hello ", br, " ", ne, " world")
+	var hell = "Hello "
+	var name string
+	fmt.Println("please enter your name:")
+	fmt.Scanf("%s", &name)
+	fmt.Println("How is the weather today?")
 }
